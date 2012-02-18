23519924efca8d866d9760a85fc4e90e64f4ed21
Pascual <Pascual@Mac-mini-de-Pascual.local>
2012-02-18 19:45:51 +0100

    Initial Commit

---
+++ iCar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iCar
+//
+//  Created by Pascual on 18/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
